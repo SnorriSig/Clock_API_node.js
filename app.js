a0aba418a0d5e548a46e0b7e15c85580cb50236c
Snorri <snor0259@stud.kea.dk>
2022-02-22 22:37:30 +0100

    nav-links

--- app.js
+++ app.js
@@ -11,29 +11,49 @@ const nav = fs.readFileSync("./public/components/nav.html").toString()
 const main = fs.readFileSync("./public/main/index.html").toString()
 const mainPage = nav.replace("%%DOCUMENT_TITLE%%", "World Clock") + main
 
+const ny = fs.readFileSync("./public/cities/new_york.html").toString()
+const nyPage = nav.replace("%%DOCUMENT_TITLE%%", "New York Time") + ny
+
+const rvk = fs.readFileSync("./public/cities/reykjavik.html").toString()
+const rvkPage = nav.replace("%%DOCUMENT_TITLE%%", "Reykjavik Time") + rvk
+
+const cph = fs.readFileSync("./public/cities/copenhagen.html").toString()
+const cphPage = nav.replace("%%DOCUMENT_TITLE%%", "Copenhagen Time") + cph
+
+const mos = fs.readFileSync("./public/cities/moscow.html").toString()
+const mosPage = nav.replace("%%DOCUMENT_TITLE%%", "Moscow Time") + mos
+
+const tok = fs.readFileSync("./public/cities/tokyo.html").toString()
+const tokPage = nav.replace("%%DOCUMENT_TITLE%%", "Tokyo Time") + tok
+
 app.get("/", (req, res) =>{
     //res.sendFile(__dirname + "/public/main/index.html")
     res.send(mainPage)
 });
 
 app.get("/new_york", (req, res) => {
-    res.sendFile(__dirname + "/public/cities/new_york.html")
+    //res.sendFile(__dirname + "/public/cities/new_york.html")
+    res.send(nyPage)
 });
 
 app.get("/reykjavik", (req, res) => {
-    res.sendFile(__dirname + "/public/cities/reykjavik.html")
+    //res.sendFile(__dirname + "/public/cities/reykjavik.html")
+    res.send(rvkPage)
 });
 
 app.get("/copenhagen", (req, res) => {
-    res.sendFile(__dirname + "/public/cities/copenhagen.html");
+    //res.sendFile(__dirname + "/public/cities/copenhagen.html");
+    res.send(cphPage)
 });
 
 app.get("/moscow", (req, res) => {
-    res.sendFile(__dirname + "/public/cities/moscow.html");
+    //res.sendFile(__dirname + "/public/cities/moscow.html");
+    res.send(mosPage)
 });
 
 app.get("/tokyo", (req, res) => {
-    res.sendFile(__dirname + "/public/cities/tokyo.html");
+    //res.sendFile(__dirname + "/public/cities/tokyo.html");
+    res.send(tokPage)
 });
 
 app.listen(port, () => {
