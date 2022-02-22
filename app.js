3c69b78b638fdc104bdc99d133fe2de546361167
Snorri <snor0259@stud.kea.dk>
2022-02-22 21:59:01 +0100

    background images

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
-const path = require('path')
 const express = require("express");
 const port = process.env.PORT || 8080
-const { log } = require('console');
 const app = express();
 
-// const publicDirectoryPath = path.join(__dirname,"/public")
-// app.use(express.static(publicDirectoryPath))
+app.use(express.static("public"));
+
+const fs = require("fs");
+
+const nav = fs.readFileSync("./public/components/nav.html").toString()
+
+const main = fs.readFileSync("./public/main/index.html").toString()
+const mainPage = nav.replace("%%DOCUMENT_TITLE%%", "World Clock") + main
 
 app.get("/", (req, res) =>{
-    res.sendFile(__dirname + "/public/index.html")
+    //res.sendFile(__dirname + "/public/main/index.html")
+    res.send(mainPage)
 });
 
 app.get("/new_york", (req, res) => {
-    res.sendFile(__dirname + "/public/new_york.html")
+    res.sendFile(__dirname + "/public/cities/new_york.html")
 });
 
 app.get("/reykjavik", (req, res) => {
-    res.sendFile(__dirname + "/public/reykjavik.html")
+    res.sendFile(__dirname + "/public/cities/reykjavik.html")
 });
 
 app.get("/copenhagen", (req, res) => {
-    res.sendFile(__dirname + "/public/copenhagen.html");
+    res.sendFile(__dirname + "/public/cities/copenhagen.html");
 });
 
 app.get("/moscow", (req, res) => {
-    res.sendFile(__dirname + "/public/moscow.html");
+    res.sendFile(__dirname + "/public/cities/moscow.html");
 });
 
 app.get("/tokyo", (req, res) => {
-    res.sendFile(__dirname + "/public/tokyo.html");
+    res.sendFile(__dirname + "/public/cities/tokyo.html");
 });
 
 app.listen(port, () => {
